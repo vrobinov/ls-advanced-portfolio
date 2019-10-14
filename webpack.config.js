03f61bf64c498aa325cf5fcf2b9cbd5dcae9aa9f
Vadim <rob.hood2012@yandex.ru>
2019-10-15 00:35:33 +0300

    docs folder added

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = (env, argv) => {
       admin: "./src/admin/main.js"
     },
     output: {
-      path: path.resolve(__dirname, "./dist"),
+      path: path.resolve(__dirname, "./docs"),
       filename: "[name].[hash].build.js",
       publicPath: isProductionBuild ? publicPath : "",
       chunkFilename: "[chunkhash].js"
