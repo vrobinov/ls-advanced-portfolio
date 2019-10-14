854a8edfd356818cab41686d114d28b057c6f036
Vadim <rob.hood2012@yandex.ru>
2019-10-15 00:38:58 +0300

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = (env, argv) => {
       admin: "./src/admin/main.js"
     },
     output: {
-      path: path.resolve(__dirname, "./docs"),
+      path: path.resolve(__dirname, "./dist"),
       filename: "[name].[hash].build.js",
       publicPath: isProductionBuild ? publicPath : "",
       chunkFilename: "[chunkhash].js"
