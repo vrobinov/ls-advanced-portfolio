ced67789f765ee760da8253715528c6bff38c5ab
Vadim <rob.hood2012@yandex.ru>
2019-10-22 22:40:00 +0300

    начал делать виджет скиллов

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,24 @@
-console.log('this is skills module');
+import Vue from "vue"
+
+const skill = {
+    tamplate:"#skill",
+    props: ["SkillName", "SkillPercent"]
+};
+
+const skillsRow = {
+    template:"#skills-row",
+    components: { skill },
+    props: ["skill"]
+};
+
+new Vue({
+    el: "#skills-component",
+    template: "#skills-list",
+    data: () => ({
+        skills: []
+    }),
+    components: { skillsRow },
+    created() {
+        this.skills = require("../data/skills.json");
+    }
+}); 
